705ed8e0cdae7df152477bc79a6a903ef7027f27
ppanagiotisilv <ppanagiotisilv@gmail.com>
2025-06-04 08:55:42 +0000

    rearrange buttons

--- modules/shareScore.js
+++ modules/shareScore.js
@@ -1,11 +1,11 @@
 // Supabase initialization
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient('https://kwyiywrugbqvhsxyvnac.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3eWl5d3J1Z2JxdmhzeHl2bmFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NTU0MTcsImV4cCI6MjA2MzIzMTQxN30.PKNz6AFQQuV8veIObld_8E-rTnsPuPayMc0ZnbbS69M')
 
 
-async function getResults(params) {
+async function getResults() {
 
   const { data, error } = await supabase
     .from('scoreboard')
@@ -16,12 +16,12 @@ async function getResults(params) {
     console.error(error);
   }
   else {
-    console.log(data);
+    renderSupabaseStats(data);
   }
 }
 
 //getResults();
-async function shareScore(params) {
+async function shareScore() {
   const localStorage = window.localStorage;
   const gameHistory = JSON.parse(localStorage.getItem('GameHistory'));
   const { data, error } = await supabase
@@ -35,12 +35,33 @@ async function shareScore(params) {
 
   if (error) {
     console.error(error);
+    alert("Your score couldn't be saved.");
   }
   else {
-    console.log(data);
+    alert('Your score has been saved.');
   }
 };
 
-/* getResults();
-shareScore(); 
-*/
+function renderSupabaseStats(params) {
+  const scoreboard = document.getElementById('scoreboard-body');
+  if (!scoreboard) return; // Αν το στοιχείο δεν υπάρχει, μην κάνεις τίποτα
+  scoreboard.innerHTML = ''; // Καθαρισμός προηγούμενων εγγραφών
+
+  if (params.length === 0) {
+    const row = scoreboard.insertRow();
+    const cell = row.insertCell();
+    cell.colSpan = 3; // Ο αριθμός των στηλών του πίνακα
+    cell.textContent = 'No saved games';
+    cell.classList.add('text-center');
+    return;
+  }
+
+  params.forEach((entry) => {
+    const row = scoreboard.insertRow();
+    row.insertCell().textContent = entry.Player||"Player";
+    row.insertCell().textContent = entry.Opponent;
+    row.insertCell().textContent = entry.Score;
+  });
+}
+
+export {getResults, shareScore};
